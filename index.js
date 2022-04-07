9c245ccf62cb3be641f87777aa06c6b792f15a24
Milla Trogen <milla.trogen@tuni.fi>
2022-04-07 11:35:23 +0300

    add test.http , update index.js to use database connection

--- index.js
+++ index.js
@@ -5,12 +5,32 @@ require("dotenv").config();
 const mysql = require("mysql");
 const port = process.env.PORT || 8080;
 
-const db = [{ name: "tiina" }, { name: "jack" }];
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+};
 
-app.get("/names", (req, res) => {
-  res.send(db);
+var connection = mysql.createConnection(config);
+
+app.get("/", (req, res) => {
+  connection.query("SELECT * from locations", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 const server = app.listen(port, () => {
   console.log(`Listening on port ${server.address().port}`);
 });
+
+/*
+const db = [{ name: "tiina" }, { name: "jack" }];
+
+app.get("/names", (req, res) => {
+  res.send(db);
+});*/
